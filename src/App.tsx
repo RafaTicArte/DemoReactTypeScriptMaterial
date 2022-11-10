ddc3d35522d0e98f03e52d70e54f9b5ce019dce1
RafaTicArte <rafa@ticarte.com>
2022-11-10 16:46:02 +0100

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Registered from './components/Registered';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.css';
+
+interface UserLogin {
+  email: String;
+  password: String;
+}
+
+const theme = createTheme();
+
+const App = (): JSX.Element => {
+  const [registered, setRegistered] = useState<Boolean>(false);
+  const [user, setUSer] = useState<UserLogin>({ email: '', password: '' });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography component="h1" variant="h3" align="center" color="text.primary" gutterBottom>
+        Demo React with TypeScript and Material UI
+      </Typography>
+      {registered === true ? (
+        <Registered data={user} onRegisteredBack={() => setRegistered(false)} />
+      ) : (
+        <Form
+          onFormSubmit={(data: UserLogin) => {
+            setRegistered(true);
+            setUSer(data);
+          }}
+        />
+      )}
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Registered.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import { Button, Typography } from '@mui/material';
+
+interface UserLogin {
+  email: String;
+  password: String;
+}
+
+interface RegisteredProps {
+  data: UserLogin;
+  onRegisteredBack: Function;
+}
+
+const Registered = ({ data, onRegisteredBack }: RegisteredProps): JSX.Element => {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Typography component="h1" variant="h5" align="center">
+        Registered
+      </Typography>
+      <Typography component="p" variant="body1" align="center">
+        {data.email}
+      </Typography>
+      <Typography component="p" variant="body1" align="center">
+        {data.password}
+      </Typography>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={() => onRegisteredBack()}
+      >
+        Back
+      </Button>
+    </Container>
+  );
+};
+
+export default Registered;
